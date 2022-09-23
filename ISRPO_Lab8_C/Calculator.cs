ced18e376d47c69ffb71598f48b65edd3a32d48c
Kirill <bunneypass@gmail.com>
2022-09-23 14:20:29 +0300

    initial commit

---
+++ ISRPO_Lab8_C/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ISRPO_Lab8_C
+{
+    public class Calculator
+    {
+        public static double Calculate(string query)
+        {
+            try
+            {
+                return Convert.ToDouble(new DataTable().Compute(query.Replace(',', '.'), null));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return 0;
+            }
+        }
+    }
+}
---
+++ ISRPO_Lab8_C/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace ISRPO_Lab8_C
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (Button btn in uniformGrid.Children)
+            {
+                btn.Click += Btn_Click;
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Button btn = toolBar.Items[i] as Button;
+                btn.Click += Btn_Click;
+            }
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text += (sender as Button).Content;
+        }
+
+        private void BtnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = Calculator.Calculate(textBlock.Text).ToString();
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = "";
+        }
+    }
+}
